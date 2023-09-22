6438a18e4287cd7453930eea95e39764ebb4741f
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-09-22 17:35:36 +0200

    revert

--- chia/consensus/constants.py
+++ chia/consensus/constants.py
@@ -4,7 +4,7 @@ import dataclasses
 import logging
 from typing import Any
 
-from chia.types.blockchain_format.sized_bytes import bytes20, bytes32
+from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.ints import uint8, uint32, uint64, uint128
 
